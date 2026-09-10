0cce1d34a706dabaef95ab59c00135b429406b48
Dom Rodriguez <shymega@shymega.org.uk>
2026-09-10 23:58:56 +0100

    refactor: Install 1Password from upstream repo

---
+++ files/scripts/_1password_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://downloads.1password.com/linux/keys/1password.asc" > /etc/yum.repos.d/1password.repo
+
+dnf install 1password -y
